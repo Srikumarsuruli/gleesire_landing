d5f3218393aaa726346a7bc1f320d6f752266eea
Srikumarsuruli <srikumarbe97@gmail.com>
2025-12-18 02:51:13 +0530

    bug

--- sticky-header.js
+++ sticky-header.js
@@ -5,11 +5,17 @@ window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     
     if (scrollTop > lastScrollTop && scrollTop > 100) {
-        // Scrolling down
+        // Scrolling down - hide header but keep cs_gescout_show
         header.classList.add('hidden');
+        if (!header.classList.contains('cs_gescout_show')) {
+            header.classList.add('cs_gescout_show');
+        }
     } else {
-        // Scrolling up
+        // Scrolling up - show header and keep cs_gescout_show
         header.classList.remove('hidden');
+        if (!header.classList.contains('cs_gescout_show')) {
+            header.classList.add('cs_gescout_show');
+        }
     }
     
     lastScrollTop = scrollTop;
